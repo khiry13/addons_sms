66bf34ec6c5c39570c79efe8e0dc8d2bfed0416e
khiry13 <ahmedkhiry414@gmail.com>
2024-08-28 09:49:11 +0300

    [IMP] SMS Notifications: Implement triggers, logic, and testing for activity notifications
    - Defined notification triggers for key activities such as course enrollment.
    - Implemented Python logic using Odoo's `mail.activity` to generate and send notifications based on defined triggers.
    - Ensured notifications include relevant details, such as student name, course enrolled, and actions taken.
    conducteconducted testing to verify that notifications are triggered correctly and delivered to the intended recipients, including registrars and teachers.

--- sms_module/models/enrollment.py
+++ sms_module/models/enrollment.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from odoo import models, fields, api
 from odoo import _, exceptions
 from odoo.exceptions import ValidationError
@@ -64,12 +66,7 @@ class Enrollment(models.Model):
     # endregion
 
     # region ---------------------- TODO[IMP]: CRUD Methods -------------------------------------
-    # endregion
-
-    # region ---------------------- TODO[IMP]: Action Methods -------------------------------------
-    # endregion
-
-    # region ---------------------- TODO[IMP]: Business Methods -------------------------------------
+    @api.model
     def create(self, vals):
         res = super(Enrollment, self).create(vals)
         student = res.student_id
@@ -79,5 +76,27 @@ class Enrollment(models.Model):
 
         student.message_post(body=message)
 
+        res.schedule_enrollment_notification()
+
         return res
+    # endregion
+
+    # region ---------------------- TODO[IMP]: Action Methods -------------------------------------
+    # endregion
+
+    # region ---------------------- TODO[IMP]: Business Methods -------------------------------------
+    def schedule_enrollment_notification(self):
+        """Schedules a notification activity for the teacher about the new enrollment."""
+        if self.teacher_id:
+            # Calculate the notification deadline (enrollment_date + 2 days)
+            notification_deadline = fields.Date.from_string(self.enrollment_date) + timedelta(days=2)
+
+            # Schedule an activity for the teacher
+            self.activity_schedule(
+                'mail.mail_activity_data_todo',  # Activity type: To Do
+                date_deadline=notification_deadline,
+                user_id=self.teacher_id.id,
+                summary=_("New student enrollment confirmed: %s") % (self.student_id.name)
+            )
+
     # endregion
